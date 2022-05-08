1742c47c7271e401e582e8d827c8dcd8c2b1a25f
vagrant <vagrant@vagrant.vm>
2022-05-08 07:51:46 +0000

    webgl-tutorial 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 import './App.css'
 import init, * as wasm from './../wasm/pkg';
+import { useEffect } from 'react';
+import { mat4 } from 'gl-matrix';
+
+const vsSource = `
+  attribute vec4 aVertexPosition;
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
+  
+  void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  }
+`;
+
+const fsSource = `
+  void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  }
+`;
 
 function App() {
   const wasmAlert = async () => {
@@ -7,9 +25,144 @@ function App() {
     wasm.greet()
   }
 
+  const loadShader = (gl: WebGLRenderingContext, type: number, source: string) => {
+    const shader = gl.createShader(type)! as WebGLShader;
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      gl.deleteShader(shader);
+      return null;
+    }
+    return shader;
+  }
+
+  const initShaderProgram = (gl: WebGLRenderingContext, vsSource: string, fsSource: string) => {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)! as WebGLShader;
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)! as WebGLShader;
+
+    const shaderProgram = gl.createProgram()! as WebGLProgram;
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      return null;
+    }
+    return shaderProgram;
+  }
+
+  const initBuffers = (gl: WebGLRenderingContext) => {
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    const positions = [
+      -1.0, 1.0,
+      1.0, 1.0,
+      -2.0, -1.0,
+      2.0, -1.0,
+      // -4.0, -1.0,
+      // 3.0, -1.0,
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    return {
+      position: positionBuffer,
+    };
+  }
+
+  const drawScene = (gl: WebGLRenderingContext, buffers: any) => {
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource)! as WebGLProgram;
+    const programInfo = {
+      program: shaderProgram,
+      attribLocations: {
+        vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      },
+      uniformLocations: {
+        projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+        modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+      },
+    };
+
+    gl.clearColor(0.3, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fieldOfView = 45 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(
+      projectionMatrix,
+      fieldOfView,
+      aspect,
+      zNear,
+      zFar,
+    );
+
+    const modelViewMatrix = mat4.create();
+    mat4.translate(
+      modelViewMatrix,
+      modelViewMatrix,
+      [-0.0, 0.0, -6.0],
+    );
+
+    {
+      const numComponents = 2;
+      const type = gl.FLOAT;
+      const normalize = false;
+      const stride = 0;
+      const offset = 0;
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+      gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset,
+      );
+      gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition,
+      );
+    }
+
+    gl.useProgram(programInfo.program);
+
+    gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix,
+    );
+    gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix,
+    );
+
+    {
+      const offset = 0;
+      const vertexCount = 4;
+      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
+  }
+
+  useEffect(() => {
+    const canvas = document.getElementById('glCanvas') as HTMLCanvasElement;
+    const gl = canvas.getContext('webgl')
+    if (gl) {
+      const buffers = initBuffers(gl);
+      drawScene(gl, buffers)
+    }
+  }, [])
+
   return (
     <div className="App">
       <button type="button" onClick={wasmAlert}>Hello Wasm!</button>
+      <canvas id="glCanvas" width="640" height="480"></canvas>
     </div>
   )
 }
